21afe1ad1d5f73f18ecd48ea88224349220366cf
cagatep <cagatep@sfu.ca>
2022-04-28 10:19:27 -0400

    Final

--- Final Project.R
+++ Final Project.R
@@ -1,9 +1,6 @@
 ### STAT 5703 - FINAL PROJECT 
 # HEART DATASET
 
-# orange = low risk of heart attack
-# blue = high risk of heart attack
-
 suppressPackageStartupMessages(library(kmed))
 suppressPackageStartupMessages(library(VGAM))
 suppressPackageStartupMessages(library(dplyr))
@@ -16,6 +13,7 @@ suppressPackageStartupMessages(library(rtkore))
 suppressPackageStartupMessages(library(Rcpp))
 
 
+# Data manipulation #### Patric this is the same as what you gave me so you don't have to copy this
 data = heart
 data$class = as.factor(ifelse(data$class>0,1,0)) # Convert to binary problem then factor
 data$sex<-as.factor(as.numeric((data$sex)))
@@ -29,46 +27,33 @@ palette<-c("#D16103","#4E84C4") # Colour palette for consistency
 heart.famd = FAMD(data, sup.var = 14, ncp = 20, graph = FALSE) # Creates FAMD coordinates
 summary(heart.famd)
 heart.eig <- get_eigenvalue(heart.famd) # Eigenvalues
-head(heart.eig, 10) ##### Patric: Check these and make sure they are the same as what is in my document
+head(heart.eig, 10) 
 
-fviz_screeplot(heart.famd, main='') ###### You can check these too
-
-#### I still can't figure out what the triangles are doing!!!! There are 16 of them. 
-# If I can't figure it out worst case scen I'll just plot it using ggplot and remove the triangles...
-fviz_famd_ind(heart.famd, label = "none", 
-              habillage = "class", palette = palette, # color by groups 
-              repel = TRUE, alpha.ind = 0.5) + 
-  theme(text = element_text(size=20), axis.text.x = element_text(size=20), axis.text.y = element_text(size=20))
-
+fviz_screeplot(heart.famd, main='') 
 
 fviz(heart.famd, "var") # Variable plot
 fviz(heart.famd, "ind", habillage = 'class', label = 'none') +
   scale_color_manual(values = palette) #Individuals plot
 
-
-
 # individual coordinates for dimensions
 famd.coords = data.frame(heart.famd$ind$coord)
 
-ggplot(aes(x=famd.coords$Dim.1, y=famd.coords$Dim.2, col=data$class))  +
-geom_vline(xintercept=0) +
-geom_hline(yintercept=0) +
-geom_point() +
-scale_color_manual(values = palette)
 
 ####### K-Prototype ######
 set.seed(123)
-heart.kproto = data |> select(-class)
+heart.kproto = data |> select(-class) # removing class (response) variable
 
 k = 10 # Max number of clusters to try
-kproto.wss = data.frame(matrix(data=NA, nrow = k, ncol=2))
+kproto.wss = data.frame(matrix(data=NA, nrow = k, ncol=2)) # Empty df for WSS results
 colnames(kproto.wss) = c('clusters', 'wss')
 
+# Calculate WSS for 2-k number of clusters 
 for (i in 2:k){ 
     kproto.wss[i,1] = i
     kproto.wss[i,2] <- sum(kproto(heart.kproto, i)$withinss)
 }
 
+# WSS graph
 kproto.wss |>
   ggplot(aes(x=clusters, y=wss)) + 
   geom_point() + 
@@ -76,8 +61,6 @@ kproto.wss |>
   scale_x_continuous(breaks=1:k) +
   xlab('Number of Clusters') + ylab('Within Sum of Squares')
 
-# FAMD coordinates for each dimension
-famd.coords = data.frame(heart.famd$ind$coord)
 
 ##### Plotting of clusters ####
 
@@ -98,7 +81,7 @@ num.clust = function(k){
   return(clust.plot)
 }
 
-  num.clust(8)
+num.clust(8)
 
 
 
